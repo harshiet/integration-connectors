0a59525887003f3cdf26f9bd17bafd0a4176c4bd
harshiet <harsh.agarwal82@gmail.com>
2015-03-12 00:12:21 -0400

    1

--- workfront-connector/src/main/java/com/aurotech/integration/connector/workfront/WorkfrontConnectorController.java
+++ workfront-connector/src/main/java/com/aurotech/integration/connector/workfront/WorkfrontConnectorController.java
@@ -3,13 +3,13 @@ package com.aurotech.integration.connector.workfront;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aurotech.integration.connector.CommonUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 @RequestMapping("/connector/workfront")
@@ -18,9 +18,9 @@ public class WorkfrontConnectorController {
 	WorkfrontService ws;
 
 	@RequestMapping(value = "/createOrUpdateTask", method = RequestMethod.POST)
-	public void createOrUpdateTask(@RequestParam(required = true) String task) throws JsonProcessingException,
-			IOException {
-		ws.createOrUpdateTask(CommonUtils.stringToJsonNode(task));
+	public void createOrUpdateTask(@RequestBody JsonNode tasks)
+			throws JsonProcessingException, IOException {
+		ws.createOrUpdateTask(tasks);
 	}
 
 }
